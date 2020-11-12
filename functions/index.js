4c991eb4f6cb543a2bfa8d2b60bb94b385bf22f6
early-riser18 <justin.warambourg@gmail.com>
2020-11-12 07:59:57 +0100

    updated based on Firebase support

--- functions/index.js
+++ functions/index.js
@@ -2,25 +2,16 @@ const firebase = require("firebase");
 const functions = require("firebase-functions");
 const auth = require("firebase/auth");
 const storage = require('firebase/storage');
+const admin = require('firebase-admin');
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAddqyJJRGL9vCNBS8IARse91gKkEhMgYM",
-    authDomain: "diving-app-eaabe.firebaseapp.com",
-    databaseURL: "https://diving-app-eaabe.firebaseio.com",
-    projectId: "diving-app-eaabe",
-    storageBucket: "diving-app-eaabe.appspot.com",
-    messagingSenderId: "718395383991",
-    appId: "1:718395383991:web:b7bbf55e5c4b41ddb0ad32",
-    measurementId: "G-V3QYZP2XX7"
-  };
 
-firebase.initializeApp(firebaseConfig);
+admin.initializeApp();
 
 
 const express = require("express");
 const app = express();
 var bodyParser = require("body-parser");
-var db = firebase.database();
+var db = admin.database();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
